f538045e74839e450e7010da5a258137f1f21e35
Anju Chaturvedi <anju.c@uplers.in>
2022-10-06 10:41:28 +0530

    react js world first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import {Navbar,NavbarBrand, Button, Container } from 'reactstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <>
+         <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Aghanya Rudra World</NavbarBrand>
+          </div>
+         </Navbar>
+         <Container>
+          
+              <h1 className="display-4">Hello, Reactstrap!</h1>
+              <p className="lead">
+                This is a simple hero unit, a simple jumbotron-style component for
+                calling extra attention to featured content or information.
+              </p>
+              <hr className="my-4" />
+              <p>
+                It uses utility classes for typography and spacing to space content
+                out within the larger container.
+              </p>
+              <Button color="primary">Learn more</Button>
+            
+    </Container>
+         </>
   );
 }
 
